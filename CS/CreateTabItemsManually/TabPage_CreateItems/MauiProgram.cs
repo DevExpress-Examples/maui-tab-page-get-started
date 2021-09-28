10b60228cf89c2859a1503e0279a416ff5ac90d4
Alexey Zhukov <alexey.zhukov@devexpress.com>
2021-09-28 12:36:06 +0300

    DevExpress Tab Page for .NET MAUI Preview 8

---
+++ CS/CreateTabItemsManually/TabPage_CreateItems/MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.Maui.Navigation;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
+
+namespace TabPage_CreateItems
+{
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+			builder
+				.UseMauiApp<App>()
+				.ConfigureMauiHandlers((handlers => handlers.AddHandler<TabPage, TabPageHandler>()))
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				});
+
+			return builder.Build();
+		}
+	}
+}
