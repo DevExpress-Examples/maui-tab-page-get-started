778f5e1a486a895446edebed06f1fc9f71d9177a
AlexeyZhukoff <alexey.zhukov@devexpress.com>
2021-09-30 16:22:36 +0300

    DevExpress Tab Page for .NET MAUI Preview 8
    * DevExpress Tab Page for .NET MAUI Preview 8
    
    * Update Readme.md
    
    * Update Readme.md

--- CS/GenerateTabItemsFromDataSource/TabPage_GenerateItems/Platforms/Android/MainApplication.cs
+++ CS/GenerateTabItemsFromDataSource/TabPage_GenerateItems/Platforms/Android/MainApplication.cs
@@ -6,11 +6,11 @@ using Microsoft.Maui;
 namespace TabPage_GenerateItems
 {
 	[Application]
-	public class MainApplication : MauiApplication<Startup>
+	public class MainApplication : MauiApplication
 	{
 		public MainApplication(IntPtr handle, JniHandleOwnership ownership)
-			: base(handle, ownership)
-		{
+			: base(handle, ownership) {
 		}
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 	}
 }
